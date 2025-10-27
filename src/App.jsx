da0160a344d3fdfdf835a375f92a826e349f7fb0
bofrank <bo@bofrank.com>
2025-10-27 19:28:29 +0900

    typos

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,21 @@
-import Form from './components/Form.jsx';
+import React from 'react';
+import Form from './components/Form';
 
 // Don't change the name of the 'App' 
 // function and keep it a named export
 
 export default function App() {
-  function handleRestart() {}
+    
+  const form = React.useRef();
+    
+  function handleRestart() {
+    form.current.clear();
+  }
 
   return (
     <div id="app">
       <button onClick={handleRestart}>Restart</button>
-      <Form />
+      <Form ref={form} />
     </div>
   );
 }
